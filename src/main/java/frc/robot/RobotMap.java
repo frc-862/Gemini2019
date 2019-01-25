3118e7b27a7115c4a00d817f0b054c1afa90d790
ianmhuff <ianmhuff@gmail.com>
2019-01-24 19:05:56 -0500

    added gemini drivetrain and drivetrain chooser
    basically just copied glitch's
    
    also added all the imports associated with such things

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,11 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -34,4 +39,30 @@ public class RobotMap {
 
   public static final int leftFlywheelCANId = 4;
   public static final int rightFlywheelCANId = 5;
+
+  public static BaseMotorController leftSlave1;
+  public static BaseMotorController leftSlave2;
+  public static BaseMotorController rightSlave1;
+  public static BaseMotorController rightSlave2;
+  public static BaseMotorController leftSlave;
+  public static BaseMotorController rightSlave;
+
+  public static void init(){
+    if (Robot.isOBot()) {
+      leftSlave1 = new WPI_TalonSRX(2);
+      leftSlave2 = new WPI_TalonSRX(3);
+      rightSlave1 = new WPI_TalonSRX(5);
+      rightSlave2 = new WPI_TalonSRX(6);
+    }
+    else if (Robot.isGlitch()) {
+      leftSlave1 = new WPI_VictorSPX(2);
+      leftSlave2 = new WPI_VictorSPX(3);
+      rightSlave1 = new WPI_VictorSPX(5);
+      rightSlave2 = new WPI_VictorSPX(6);
+    }
+   else { // if (Robot.isGemini() || Robot.isNebula())  {
+      leftSlave = new WPI_VictorSPX(2);
+      rightSlave = new WPI_VictorSPX(4);
+    }
+  }
 }
