a809b967562eacdb897d4b536d63f60e06821ba4
darren <darren.tas7@gmail.com>
2019-02-06 21:31:52 -0500

    added condition for robot to only use squint for turning when 60 in away

--- src/main/java/frc/lightning/commands/VisionDriveAndAdjust.java
+++ src/main/java/frc/lightning/commands/VisionDriveAndAdjust.java
@@ -39,21 +39,28 @@ public class VisionDriveAndAdjust extends Command {
       Target target = Robot.vision.getBestTarget();
       double squint = target.squint();
 
-      if (Math.abs(squint) > SQUINT_BOUND || Math.abs(target.rotation()) > ROTATION_BOUND) {
+      if (target.standoff() > 60){
+
+        double squintAdjustment = target.squint() * 0.005 * 0.0025; //power constants taken from the else if block directly below
+        Robot.drivetrain.setPower(.3 - squintAdjustment, .3 + squintAdjustment);
+
+      }
+
+      else if (Math.abs(squint) > SQUINT_BOUND || Math.abs(target.rotation()) > ROTATION_BOUND) {
 
           double adjustment = target.squint() * 0.005 + Math.signum(target.rotation()) * Math.pow(Math.abs(target.rotation()), 1.3) * 0.0025;
           Robot.drivetrain.setPower(.3 - adjustment, .3 + adjustment);
           //Robot.drivetrain.setPower(0.4,0.4);
           SmartDashboard.putString("vision turn status", "turning");
-          
-          
-          
-
+        
       }
+
+
+
       else if (target.standoff() > 30) {
 
           //double power = 0.035 * target.standoff() + 0.075;
-          Robot.drivetrain.setPower(0.3, 0.3);
+          Robot.drivetrain.setPower(0.25, 0.25);
           SmartDashboard.putString("vision turn status", "not turning");
           
       }
