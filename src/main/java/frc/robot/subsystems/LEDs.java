999796473f3719b2443d86afdd8021359e91a1bb
Patrick Hurley <phurley@gmail.com>
2019-03-25 23:22:54 -0400

    Add stub functions

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -18,6 +18,15 @@ import frc.robot.commands.LEDs.UpdateLEDState;
  */
 
 public class LEDs extends Subsystem {
+    public void setYellow() {
+    }
+
+    public void setGreen() {
+    }
+
+    public void setPurple() {
+    }
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
